45ee1e84192d1d61c48d57ad2c7b2bf58a5f4a3f
Ayax <casarrubias2837@gmail.com>
2020-05-15 22:37:34 -0500

    Tests and modules complete

--- app/BankAccount.js
+++ app/BankAccount.js
@@ -1,6 +1,7 @@
 class BankAccount {
     constructor(account) {
         this.account = account;
+        this.history = [];
     }
 
     current() {
@@ -8,13 +9,31 @@ class BankAccount {
     }
 
     append(account, amount){
-        this.account = account + amount;
+        if(amount >= 0){
+            this.account = account + amount;
+            
+        }
+        this.history.push({amount: account});
         return this.account;
     }
 
     substract(account, amount){
-        this.account = account - amount;
+        if(amount >= 0){
+            this.account = account - amount;
+            
+        }
+        this.history.push({amount: account});
         return this.account;
+    }
+
+    merge(client2 ) {
+        client2.account = client2.account + this.account;
+        this.history.push({amount: client2.account});
+        return this.client2;
+    }
+
+    final( ) {
+       return this.history;
     }
 
 }
--- test/test-bank-acc.js
+++ test/test-bank-acc.js
@@ -13,20 +13,60 @@ describe('BancAcc', () => {
     describe('#append', () => {
         let account = 1000;
         let amount = 1500; 
+        let amountN = -1000;
         let client = new BankAccount(account);
 
         it('Return bank account after append', () => {
+            assert.deepEqual(account, client.append(account, amountN));
             assert.deepEqual(account + amount, client.append(account, amount));
+            
         })
     })
 
     describe('#substract', () => {
         let account = 2000;
         let amount = 1500; 
+        let amountN = -1000;
         let client = new BankAccount(account);
 
         it('Return bank account after substraction', () => {
+            assert.deepEqual(account, client.append(account, amountN));
             assert.deepEqual(account - amount, client.substract(account, amount));
         })
     })
+
+    describe('#merge', () => {
+        let account1 = 2000;
+        let account2 = 1500; 
+        let client = new BankAccount(account1);
+        let client2 = new BankAccount(account2)
+        let history1 = 500;
+        let history2 = 1500;
+
+        client.append(history1);
+        client2.substract(history2);
+        client.merge(client2);
+
+        it('Return bank account after merge accounts', () => {
+            assert.deepEqual([{amount: 500}, {amount: 3500}], client.final());
+        })
+        
+    })
+
+    describe('#history', () => {
+        let account = 1500;
+        let client = new BankAccount(account);
+        let history1 = 800;
+        let history2 = 1200;
+
+        client.append(history1);
+        client.substract(history2);
+
+        it('Return bank account after merge accounts', () => {
+            assert.deepEqual([{amount: 800},{amount: 1200}], client.final());
+        })
+        
+    })
+
+
 })
